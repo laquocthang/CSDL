6da2f9e19f44e5def108ea4db653a6f46a3170e8
La Quốc Thắng <37357028+laquocthang@users.noreply.github.com>
2018-04-10 20:17:56 +0700

    Upload some files

---
+++ BaiTapThem/QL_NhapXuatHangHoa.sql
@@ -0,0 +1,135 @@
+﻿/* Môn: Cơ sở dữ liệu
+   QL_NhapXuatHangHoa
+   Thực hiện: La Quốc Thắng
+   Ngày: 11/4/2018 
+*/
+---------------------------------------------------------------------------------------
+
+create database QL_NhapXuatHangHoa
+
+use QL_NhapXuatHangHoa
+
+go
+create table HangHoa
+(
+	MaHH varchar(5) primary key,
+	TenHH varchar(30) not null unique,
+	DVT nvarchar(3) not null,
+	SoLuongTon tinyint
+)
+
+go
+create table DoiTac
+(
+	MaDT varchar(5) primary key,
+	TenDT nvarchar(30) not null,
+	DiaChi nvarchar(35),
+	DienThoai varchar(12)
+)
+
+go
+create table KhaNangCC
+(
+	MaDT varchar(5) references DoiTac(MaDT),
+	MaHH varchar(5) references HangHoa(MaHH),
+	primary key(MaDT, MaHH)
+)
+
+go
+create table HoaDon
+(
+	SoHD varchar(5) primary key,
+	NgayLapHD date not null,
+	MaDT varchar(5) references DoiTac(MaDT),
+	TongTG float
+)
+
+go
+create table CT_HoaDon
+(
+	SoHD varchar(5) references HoaDon(SoHD),
+	MaHH varchar(5) references HangHoa(MaHH),
+	primary key(SoHD, MaHH),
+	DonGia tinyint not null,
+	SoLuong tinyint not null
+)
+
+insert into HangHoa values('CPU01','CPU INTEL, CELERON 600 BOX',N'CÁI','5')
+insert into HangHoa values('CPU02','CPU INTEL, PIII 700',N'CÁI','10')
+insert into HangHoa values('CPU03','CPU AMD K7 ATHL,ON 600',N'CÁI','8')
+insert into HangHoa values('HDD01','HDD 10.2 GB QUANTUM',N'CÁI','10')
+insert into HangHoa values('HDD02','HDD 13.6 GB SEAGATE',N'CÁI','15')
+insert into HangHoa values('HDD03','HDD 20 GB QUANTUM',N'CÁI','6')
+insert into HangHoa values('KB01','KB GENIUS',N'CÁI','12')
+insert into HangHoa values('KB02','KB MITSUMIMI',N'CÁI','5')
+insert into HangHoa values('MB01','GIGABYTE CHIPSET INTEL',N'CÁI','10')
+insert into HangHoa values('MB02','ACOPR BX CHIPSET VIA',N'CÁI','10')
+insert into HangHoa values('MB03','INTEL PHI CHIPSET INTEL',N'CÁI','10')
+insert into HangHoa values('MB04','ECS CHIPSET SIS',N'CÁI','10')
+insert into HangHoa values('MB05','ECS CHIPSET VIA',N'CÁI','10')
+insert into HangHoa values('MNT01','SAMSUNG 14" SYNCMASTER',N'CÁI','5')
+insert into HangHoa values('MNT02','LG 14"',N'CÁI','5')
+insert into HangHoa values('MNT03','ACER 14"',N'CÁI','8')
+insert into HangHoa values('MNT04','PHILIPS 14"',N'CÁI','6')
+insert into HangHoa values('MNT05','VIEWSONIC 14"',N'CÁI','7')
+
+insert into DoiTac values('CC001','Cty TNC',N'176 BTX Q1 - TP.HCM','08.8250259')
+insert into DoiTac values('CC002',N'Cty Hoàng Long','15A TTT Q1 - TP.HCM','08.8250898')
+insert into DoiTac values('CC003',N'Cty Hợp Nhất','152 BTX Q1 - TP.HCM','08.8252376')
+insert into DoiTac values('K0001',N'Nguyễn Minh Hải',N'91 Nguyễn Văn Trỗi Tp.Đà Lạt','063.831129')
+insert into DoiTac values('K0002',N'Như Quỳnh',N'21 Điện Biên Phủ. N.Trang','058590270')
+insert into DoiTac values('K0003',N'Trần Nhật Duật',N'Lê Lợi Tp.Huế','054.848376')
+insert into DoiTac values('K0004',N'Phan Nguyễn Hùng Anh',N'11 Nam Kỳ Khỡi Nghĩa Tp.Đà Lạt','063.823409')
+
+set dateformat dmy	--Chỉnh định dạng ngày lại
+insert into HoaDon values('N0001','25/1/2006','CC001','')
+insert into HoaDon values('N0002','1/5/2006','CC002','')
+insert into HoaDon values('X0001','12/5/2006','K0001','')
+insert into HoaDon values('X0002','16/6/2006','K0002','')
+insert into HoaDon values('X0003','20/4/2006','K0003','')
+
+insert into KhaNangCC values('CC001','CPU01')
+insert into KhaNangCC values('CC001','HDD03')
+insert into KhaNangCC values('CC001','KB01')
+insert into KhaNangCC values('CC001','MB02')
+insert into KhaNangCC values('CC001','MB04')
+insert into KhaNangCC values('CC001','MNT01')
+insert into KhaNangCC values('CC002','CPU01')
+insert into KhaNangCC values('CC002','CPU02')
+insert into KhaNangCC values('CC002','CPU03')
+insert into KhaNangCC values('CC002','KB02')
+insert into KhaNangCC values('CC002','MB01')
+insert into KhaNangCC values('CC002','MB05')
+insert into KhaNangCC values('CC002','MNT03')
+insert into KhaNangCC values('CC003','HDD01')
+insert into KhaNangCC values('CC003','HDD02')
+insert into KhaNangCC values('CC003','HDD03')
+insert into KhaNangCC values('CC003','MB03')
+
+insert into CT_HoaDon values('N0001','CPU01','63','10')
+insert into CT_HoaDon values('N0001','HDD03','97','7')
+insert into CT_HoaDon values('N0001','KB01','3','5')
+insert into CT_HoaDon values('N0001','MB02','57','5')
+insert into CT_HoaDon values('N0001','MNT01','112','3')
+insert into CT_HoaDon values('N0002','CPU02','115','3')
+insert into CT_HoaDon values('N0002','KB02','5','7')
+insert into CT_HoaDon values('N0002','MNT03','111','5')
+insert into CT_HoaDon values('X0001','CPU01','67','2')
+insert into CT_HoaDon values('X0001','HDD03','100','2')
+insert into CT_HoaDon values('X0001','KB01','5','2')
+insert into CT_HoaDon values('X0001','MB02','62','1')
+insert into CT_HoaDon values('X0002','CPU01','67','1')
+insert into CT_HoaDon values('X0002','KB02','7','3')
+insert into CT_HoaDon values('X0002','MNT01','115','2')
+insert into CT_HoaDon values('X0003','CPU01','67','1')
+insert into CT_HoaDon values('X0003','MNT03','115','2')
+
+select * from HangHoa
+
+select * from DoiTac
+
+select * from KhaNangCC
+
+select * from HoaDon
+
+select * from CT_HoaDon
---
+++ BaiTapThem/Ql_DatBao.sql
@@ -0,0 +1,76 @@
+﻿/* Môn: Cơ sở dữ liệu
+   QL_DatBao
+   Thực hiện: La Quốc Thắng
+   Ngày: 11/4/2018 
+*/
+---------------------------------------------------------------------------------------
+
+create database QL_DatBao
+
+use QL_DatBao
+
+go
+create table BAO_TCHI
+(MA_BAO_TC varchar(4) primary key,
+TEN nvarchar(30) not null,
+DINH_KY nvarchar(30) not null,
+SOLUONG smallint not null,
+GIABAN smallint not null)
+
+go
+create table PHATHANH_BAO
+(MA_BAO_TC varchar(4),
+SO_BAO_TC smallint,
+NGAY_PH date not null,
+primary key(MA_BAO_TC,SO_BAO_TC))
+
+go
+create table KH_HANG
+(MAKH varchar(4) primary key,
+TENKH nvarchar(8) not null,
+DC_KH varchar(10) not null)
+
+go
+create table DATBAO
+(MAKH varchar(4) references KH_HANG(MAKH),
+MA_BAO_TC varchar(4) references BAO_TCHI(MA_BAO_TC),
+SL_MUA smallint not null,
+NGAY_DM date not null,
+primary key(MAKH, MA_BAO_TC))
+
+insert into BAO_TCHI values('TT01',N'Tuổi trẻ',N'Nhật báo','1000','1500')
+insert into BAO_TCHI values('KT01',N'Kiến thức ngày nay',N'Bán nguyệt san','3000','6000')
+insert into BAO_TCHI values('TN01',N'Thanh niên',N'Nhật báo','1000','2000')
+insert into BAO_TCHI values('PN01',N'Phụ nữ',N'Tuần báo','2000','4000')
+insert into BAO_TCHI values('PN02',N'Phụ nữ',N'Nhật báo','1000','2000')
+
+set dateformat dmy
+insert into PHATHANH_BAO values('TT01','123','15/12/2005')
+insert into PHATHANH_BAO values('KT01','70','15/12/2005')
+insert into PHATHANH_BAO values('TT01','124','16/12/2005')
+insert into PHATHANH_BAO values('TN01','256','17/12/2005')
+insert into PHATHANH_BAO values('PN01','45','23/12/2005')
+insert into PHATHANH_BAO values('PN02','111','18/12/2005')
+insert into PHATHANH_BAO values('PN02','112','19/12/2005')
+insert into PHATHANH_BAO values('TT01','125','17/12/2005')
+insert into PHATHANH_BAO values('PN01','46','30/12/2005')
+
+insert into KH_HANG values('KH01',N'LAN','2 NCT')
+insert into KH_HANG values('KH02',N'NAM','32 THĐ')
+insert into KH_HANG values('KH03',N'NGỌC','16 LHP')
+
+insert into DATBAO values('KH01','TT01','100','12/1/2000')
+insert into DATBAO values('KH02','TN01','150','1/5/2001')
+insert into DATBAO values('KH01','PN01','200','25/6/2001')
+insert into DATBAO values('KH03','KT01','50','17/3/2002')
+insert into DATBAO values('KH03','PN02','200','26/8/2003')
+insert into DATBAO values('KH02','TT01','250','15/1/2004')
+insert into DATBAO values('KH01','KT01','300','14/10/2004')
+
+select * from BAO_TCHI
+
+select * from PHATHANH_BAO
+
+select * from KH_HANG
+
+select * from DATBAO
