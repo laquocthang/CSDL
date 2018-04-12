d41101f5719f72ca722d5a144318a894f8b42ab8
La Quốc Thắng <37357028+laquocthang@users.noreply.github.com>
2018-04-12 20:04:51 +0700

    Update some files

--- BaiTapThem/QL_NhapXuatHangHoa.sql
+++ BaiTapThem/QL_NhapXuatHangHoa.sql
@@ -1,6 +1,7 @@
 ﻿/* Môn: Cơ sở dữ liệu
-   QL_NhapXuatHangHoa
+   Bài lab: QL_NhapXuatHangHoa
    Thực hiện: La Quốc Thắng
+   MSSV: 1610207
    Ngày: 11/4/2018 
 */
 ---------------------------------------------------------------------------------------
@@ -50,7 +51,7 @@ create table CT_HoaDon
 	SoHD varchar(5) references HoaDon(SoHD),
 	MaHH varchar(5) references HangHoa(MaHH),
 	primary key(SoHD, MaHH),
-	DonGia tinyint not null,
+	DonGia int not null,
 	SoLuong tinyint not null
 )
 
@@ -86,7 +87,7 @@ insert into HoaDon values('N0001','25/1/2006','CC001','')
 insert into HoaDon values('N0002','1/5/2006','CC002','')
 insert into HoaDon values('X0001','12/5/2006','K0001','')
 insert into HoaDon values('X0002','16/6/2006','K0002','')
-insert into HoaDon values('X0003','20/4/2006','K0003','')
+insert into HoaDon values('X0003','20/4/2006','K0001','')
 
 insert into KhaNangCC values('CC001','CPU01')
 insert into KhaNangCC values('CC001','HDD03')
@@ -133,3 +134,162 @@ select * from KhaNangCC
 select * from HoaDon
 
 select * from CT_HoaDon
+
+----------------------------TRUY VẤN DỮ LIỆU--------------------------------
+
+--Câu 1: Liệt kê các mặt hàng thuộc loại đĩa cứng
+select *
+from HangHoa
+where MaHH like 'HDD%'
+
+--Câu 2: Liệt kê các mặt hàng có số lượng tồn trên 10
+select *
+from HangHoa
+where SoLuongTon>10
+
+--Câu 3: Cho biết thông tin của các nhà cung cấp ở Tp. Hồ Chí Minh
+select *
+from DoiTac
+where DiaChi like '%HCM' and MaDT like 'CC%'
+
+--Câu 4: Liệt kê các hóa đơn nhập hàng trong tháng 5/2006
+select a.SoHD, NgayLapHD, TenDT, DiaChi, DienThoai, SUM(SoLuong)as SoMatHang
+from HoaDon a, DoiTac b, CT_HoaDon c 
+where a.MaDT=b.MaDT and a.SoHD=c.SoHD and MONTH(NgayLapHD)=5 and YEAR(NgayLapHD)=2006 and a.SoHD like 'N%'
+group by a.SoHD, TenDT, DiaChi, DienThoai, NgayLapHD
+
+--Câu 5: Cho biết tên các nhà cung cấp có cung cấp đĩa cứng
+select a.MaDT, TenDT
+from DoiTac a, KhaNangCC b
+where a.MaDT=b.MaDT and b.MaHH like 'HDD%' and a.MaDT like 'CC%'
+group by a.MaDT, TenDT
+
+--Câu 6: Cho biết tên các nhà cung cấp có thể cung cấp các loại đĩa cứng
+select c.MaDT, TenDT
+from HangHoa a, KhaNangCC b, DoiTac c
+where a.MaHH=b.MaHH and c.MaDT=b.MaDT and a.MaHH like 'HDD%'
+group by c.MaDT, TenDT
+having COUNT(a.MaHH) = (select count(MaHH)
+						from HangHoa
+						where MaHH like 'HDD%')
+
+--Câu 7: Cho biết tên các nhà cung cấp không cung cấp đĩa cứng
+select a.MaDT, TenDT, DiaChi, DienThoai
+from DoiTac a, KhaNangCC b
+where a.MaDT=b.MaDT and a.MaDT not in (select MaDT
+										from KhaNangCC
+										where MaHH like 'HDD%'
+										group by MaDT)
+group by a.MaDT, TenDT, DiaChi, DienThoai										
+
+--Câu 8: Cho biết thông tin của mặt hàng chưa bán được
+select *
+from HangHoa
+where MaHH not in (select MaHH
+					from CT_HoaDon
+					group by MaHH)
+
+--Câu 9: Cho biết tên và tổng số lượng bán của mặt hàng bán chạy nhất
+select TenHH, SUM(SoLuong) as TongSLBan
+from HangHoa a, CT_HoaDon b
+where a.MaHH=b.MaHH and SoHD like 'X%'
+group by TenHH
+having SUM(SoLuong)>=all(select SUM(SoLuong)
+						from CT_HoaDon
+						where SoHD like 'X%'
+						group by MaHH)
+
+--Câu 10: Cho biết tên và tổng số lượng của mặt hàng nhập về ít nhất
+select TenHH, SUM(SoLuong) as TongSLBan
+from HangHoa a, CT_HoaDon b
+where a.MaHH=b.MaHH and SoHD like 'N%'
+group by TenHH
+having SUM(SoLuong)<=all(select SUM(SoLuong)
+						from CT_HoaDon
+						where SoHD like 'N%'
+						group by MaHH)
+
+--Câu 11: Cho biết hóa đơn nhập nhiều mặt hàng nhất
+select a.SoHD, NgayLapHD, SUM(SoLuong) as TongSLNhap
+from HoaDon a, CT_HoaDon b
+where a.SoHD=b.SoHD and b.SoHD like 'N%'
+group by a.SoHD, NgayLapHD
+having SUM(SoLuong) >= all(select SUM(SoLuong)
+							from CT_HoaDon
+							where SoHD like 'N%'
+							group by SoHD)
+
+--Câu 12: Cho biết các mặt hàng không được nhập hàng trong tháng 1/2006
+select a.MaHH, TenHH
+from HangHoa a, CT_HoaDon b
+where a.MaHH=b.MaHH and a.MaHH not in (select MaHH
+										from CT_HoaDon d, HoaDon e
+										where d.SoHD=e.SoHD and e.SoHD like 'N%' and MONTH(NgayLapHD)=1 and YEAR(NgayLapHD)=2006
+										group by MaHH)
+
+--Câu 13: Cho biết các mặt hàng không bán được trong tháng 6/2006
+select a.MaHH, TenHH
+from HangHoa a, CT_HoaDon b
+where a.MaHH=b.MaHH and a.MaHH not in (select MaHH
+										from CT_HoaDon d, HoaDon e
+										where d.SoHD=e.SoHD and e.SoHD like 'X%' and MONTH(NgayLapHD)=6 and YEAR(NgayLapHD)=2006
+										group by MaHH)
+
+--Câu 14: Cho biết cửa hàng bán bao nhiêu mặt hàng
+select COUNT(SoHD) as TongSLBan
+from CT_HoaDon
+where SoHD like 'X%'
+
+--Câu 15: Cho biết số mặt hàng mà từng nhà cung cấp có khả năng cung cấp
+select A.MaDT, TenDT, COUNT(MaHH)
+from DoiTac a, KhaNangCC B
+where a.MaDT=B.MaDT
+group by a.MaDT, TenDT
+
+--Câu 16: Cho biết thông tin của khách hàng có giao dịch với cửa hàng nhiều nhất
+select a.MaDT, TenDT, DiaChi, DienThoai
+from DoiTac a, HoaDon b
+where a.MaDT=b.MaDT and a.MaDT like 'K%'
+group by a.MaDT, TenDT, DiaChi, DienThoai
+having COUNT(b.SoHD)>=all (select COUNT(SoHD)
+						from HoaDon
+						group by MaDT)
+
+--Câu 17: Tính tổng doanh thu năm 2006
+select sum(DonGia * SoLuong) as TongDoanhThu
+from HoaDon a, CT_HoaDon b
+where a.SoHD=b.SoHD and Year(NgayLapHD)=2006
+
+--Câu 18: Cho biết loại mặt hàng bán chạy nhất
+select a.MaHH, TenHH
+from HangHoa a, CT_HoaDon b
+where a.MaHH=b.MaHH and SoHD like 'X%'
+group by a.MaHH, TenHH
+having SUM(SoLuong) >= all (select SUM(SoLuong)
+							from CT_HoaDon
+							where SoHD like 'X%'
+							group by MaHH)
+
+--Câu 19: Liệt kê thông tin bán hàng của tháng 5/2006
+select a.MaHH, TenHH, DVT, SUM(SoLuong) as TongSoLuong, SUM(DonGia * SoLuong) as TongThanhTien
+from HangHoa a, CT_HoaDon b, HoaDon c
+where a.MaHH=b.MaHH and b.SoHD=c.SoHD and b.SoHD like 'X%' and MONTH(NgayLapHD)=5 and YEAR(NgayLapHD)=2006
+group by a.MaHH, TenHH, DVT
+
+--Câu 20: Liệt kê thông tin mặt hàng có nhiều người mua nhất
+select a.MaHH, TenHH
+from HangHoa a, CT_HoaDon b
+where a.MaHH=b.MaHH and SoHD like 'X%'
+group by a.MaHH, TenHH
+having SUM(SoLuong) >= all (select SUM(SoLuong)
+							from CT_HoaDon
+							where SoHD like 'X%'
+							group by MaHH)
+
+--Câu 21: Tính và cập nhật tổng trị giá của các hóa đơn
+update HoaDon	
+set TongTG=(select SUM(DonGia * SoLuong)
+			from CT_HoaDon
+			where CT_HoaDon.SoHD=HoaDon.SoHD)
+
+select * from HoaDon
--- BaiTapThem/Ql_DatBao.sql
+++ BaiTapThem/Ql_DatBao.sql
@@ -1,6 +1,7 @@
 ﻿/* Môn: Cơ sở dữ liệu
-   QL_DatBao
+   Bài lab: QL_DatBao
    Thực hiện: La Quốc Thắng
+   MSSV: 1610207
    Ngày: 11/4/2018 
 */
 ---------------------------------------------------------------------------------------
@@ -11,32 +12,40 @@ use QL_DatBao
 
 go
 create table BAO_TCHI
-(MA_BAO_TC varchar(4) primary key,
-TEN nvarchar(30) not null,
-DINH_KY nvarchar(30) not null,
-SOLUONG smallint not null,
-GIABAN smallint not null)
+(
+	MA_BAO_TC varchar(4) primary key,
+	TEN nvarchar(30) not null,
+	DINH_KY nvarchar(30) not null,
+	SOLUONG smallint not null,
+	GIABAN int not null
+)
 
 go
 create table PHATHANH_BAO
-(MA_BAO_TC varchar(4),
-SO_BAO_TC smallint,
-NGAY_PH date not null,
-primary key(MA_BAO_TC,SO_BAO_TC))
+(
+	MA_BAO_TC varchar(4),
+	SO_BAO_TC smallint,
+	NGAY_PH date not null,
+	primary key(MA_BAO_TC,SO_BAO_TC)
+)
 
 go
 create table KH_HANG
-(MAKH varchar(4) primary key,
-TENKH nvarchar(8) not null,
-DC_KH varchar(10) not null)
+(
+	MAKH varchar(4) primary key,
+	TENKH nvarchar(8) not null,
+	DC_KH varchar(10) not null
+)
 
 go
 create table DATBAO
-(MAKH varchar(4) references KH_HANG(MAKH),
-MA_BAO_TC varchar(4) references BAO_TCHI(MA_BAO_TC),
-SL_MUA smallint not null,
-NGAY_DM date not null,
-primary key(MAKH, MA_BAO_TC))
+(
+	MAKH varchar(4) references KH_HANG(MAKH),
+	MA_BAO_TC varchar(4) references BAO_TCHI(MA_BAO_TC),
+	SL_MUA smallint not null,
+	NGAY_DM date not null,
+	primary key(MAKH, MA_BAO_TC)
+)
 
 insert into BAO_TCHI values('TT01',N'Tuổi trẻ',N'Nhật báo','1000','1500')
 insert into BAO_TCHI values('KT01',N'Kiến thức ngày nay',N'Bán nguyệt san','3000','6000')
@@ -74,3 +83,102 @@ select * from PHATHANH_BAO
 select * from KH_HANG
 
 select * from DATBAO
+
+-----------------------------------TRUY VẤN SỮ LIỆU--------------------------------
+
+--Câu 1: Cho biết các tờ báo, tạp chí có định kỳ phát hành hàng tuần
+select *
+from BAO_TCHI
+where DINH_KY like N'Tuần báo'
+
+--Câu 2: Cho biết thông tin về các tờ báo thuộc loại báo phụ nữ
+select a.MA_BAO_TC, TEN, DINH_KY, SOLUONG, GIABAN, SO_BAO_TC, NGAY_PH
+from BAO_TCHI a, PHATHANH_BAO b
+where a.MA_BAO_TC=b.MA_BAO_TC and a.MA_BAO_TC like 'PN%'
+
+--Câu 3: Cho biết tên các khách hàng có đặt mua báo phụ nữ, không liệt kê khách hàng trùng
+select a.MAKH, TENKH
+from KH_HANG a, DATBAO b
+where a.MAKH=b.MAKH and MA_BAO_TC like 'PN%'
+group by a.MAKH, TENKH
+
+--Câu 4: Cho biết tên các khách hàng có đặt mua tất cả các báo phụ nữ
+select a.MAKH, TENKH
+from KH_HANG a, DATBAO b
+where a.MAKH=b.MAKH and MA_BAO_TC like 'PN%'
+group by a.MAKH, TENKH
+having COUNT(a.MAKH)=(select COUNT(MAKH)
+					from DATBAO
+					where MA_BAO_TC like 'PN%')
+					
+--Câu 5: Cho biết tên các khách hàng không đặt mua báo thanh niên
+select a.MAKH, TENKH, DC_KH
+from KH_HANG a, DATBAO b
+where a.MAKH=b.MAKH and a.MAKH not in (select MAKH
+									from DATBAO
+									where MA_BAO_TC like 'TN%')
+									
+--Câu 6: Cho biết số tờ báo mã mỗi khách hàng đã đặt mua
+select a.MAKH, TENKH, DC_KH, SUM(SL_MUA) as SoLuongMua
+from KH_HANG a, DATBAO b
+where a.MAKH=b.MAKH
+group by a.MAKH, TENKH, DC_KH
+
+--Câu 7: Cho biết số khách hàng đặt mua báo trong năm 2004
+select COUNT(MAKH)
+from DATBAO
+where YEAR(NGAY_DM)=2004
+
+--Câu 8: Cho biết thông tin đặt mua báo của các khách hàng
+select TENKH, TEN, DINH_KY, SL_MUA, SL_MUA * GIABAN as SoTien
+from KH_HANG a, BAO_TCHI b, DATBAO c
+where a.MAKH=c.MAKH and b.MA_BAO_TC=c.MA_BAO_TC
+group by TENKH, TEN, DINH_KY, SL_MUA, GIABAN
+
+--Câu 9: Cho biết các tờ báo, tạp chí và tổng số lượng đặt mua của từng khách hàng đối với tờ báo, tạp chí đó
+select A.MA_BAO_TC, TEN, DINH_KY, SUM(SL_MUA) AS SoLuongDat
+from BAO_TCHI A, DATBAO B
+where A.MA_BAO_TC=B.MA_BAO_TC
+group by A.MA_BAO_TC,TEN,DINH_KY
+
+--Câu 10:Cho biết các tờ báo dành cho sv
+select MA_BAO_TC, TEN
+from BAO_TCHI
+where MA_BAO_TC like 'HS%'
+
+--Câu 11:Cho biết những tờ báo không có người đặt mua
+select A.MA_BAO_TC, TEN
+from BAO_TCHI A
+where A.MA_BAO_TC not in(select B.MA_BAO_TC
+from BAO_TCHI B, DATBAO C
+where B.MA_BAO_TC=C.MA_BAO_TC)
+
+--Câu 12:Cho biết tên, định kỳ những tờ báo có nhiều người đặt mua nhất
+select TEN, DINH_KY, SUM(SL_MUA) AS TóngSLMua
+from BAO_TCHI A, DATBAO B
+where A.MA_BAO_TC=B.MA_BAO_TC
+group by  TEN,DINH_KY
+having SUM(SL_MUA)>= all(select SUM(D.SL_MUA)
+						from DATBAO D
+						group by  MA_BAO_TC)
+
+--Câu 13:Cho biết khách hàng đặt mua nhiều báo nhất
+select TENKH, DC_KH, SUM(SL_MUA) AS TongSLMua
+from KH_HANG A, DATBAO B
+where A.MAKH=B.MAKH
+group by TENKH, DC_KH
+having SUM(SL_MUA)>= all(select SUM(C.SL_MUA) 
+						from DATBAO C
+						group by MAKH)
+
+--Câu 14: Cho biết các tờ báo phát hành định kỳ 2 lần trong tháng
+select TEN, DINH_KY
+from BAO_TCHI
+where DINH_KY like N'Bán nguyệt san'
+
+--Câu 15: Cho biết các tờ báo có 3 khách hàng đặt mua trở lên
+select A.MA_BAO_TC, TEN, COUNT(MAKH) AS SoKhach
+from BAO_TCHI A, DATBAO B
+where A.MA_BAO_TC=b.MA_BAO_TC
+group by A.MA_BAO_TC, TEN
+having COUNT(MAKH)>=3
